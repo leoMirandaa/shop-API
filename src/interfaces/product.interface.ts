0064d38e89033f8f9bed1eb580cccce74472d5ca
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-07-21 23:11:43 -0700

    fix: add Product props
    add size and gender into Product.interface.ts
    add SizeType and GenderType into Product.interface.ts
    add size and gender into Product.ts model

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -2,8 +2,33 @@ export interface Product {
   name: string;
   description: string;
   price: number;
-  img: string;
-  status: boolean;
-  category: object;
+  img?: Img;
+  status?: boolean;
+  category: object; //shirts,pants,hoodies,hats
+  size: SizeType;
+  gender: GenderType;
+  // inStock: number;
   // user: object;
 }
+
+interface Img {
+  public_id: string;
+  url: string;
+}
+
+export enum SizeType {
+  XS = "Xtra-small",
+  SM = "Small",
+  MD = "Medium",
+  LG = "Large",
+  XL = "Extra-large",
+}
+// ["Xtra-small", "Small", "Medium", "Large", "Extra-large"]
+
+export enum GenderType {
+  MEN = "Men",
+  WOMEN = "Women",
+  KID = "Kid",
+  UNISEX = "Unisex",
+}
+// ["Men", "Women", "Kid", "Unisex"]
