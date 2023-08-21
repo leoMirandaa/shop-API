15a605fafcc22958e3d7bac43da201b241f7c459
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-08-20 23:09:46 -0700

    feat: add /users documentation

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -15,10 +15,78 @@ import {
 } from "../controllers/users";
 
 const router = Router();
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *          description: ID auto-generated
+ *        name:
+ *          type: string
+ *          description: name of the user
+ *        email:
+ *          type: string
+ *        password:
+ *          type: string
+ *        role:
+ *          type: string
+ *        address:
+ *          type: object
+ *          properties:
+ *            street:
+ *              type: string
+ *            city:
+ *              type: string
+ *            country:
+ *                type: string
+ *            state:
+ *              type: string
+ *            zip:
+ *              type: string
+ *        status:
+ *          type: boolean
+ *        createdAt:
+ *          type: string
+ *        updatedAt:
+ *          type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ *  description: Users endpoints
+ */
+
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *    tags: [Users]
+ *    description: Responds if the app is up and running
+ *    summary: Get User list
+ *    responses:
+ *      200:
+ *        description: List of Users
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/User'
+ */
 router.get("/", getUsers);
 router.get("/:id", validateGet, getUser);
-router.post("/", checkJWT, validateCreate, createUser);
-router.put("/:id", checkJWT, validateUpdate, updateUser);
-router.delete("/:id", checkJWT, validateDelete, deleteUser);
+// router.post("/", checkJWT, validateCreate, createUser);
+// router.put("/:id", checkJWT, validateUpdate, updateUser);
+// router.delete("/:id", checkJWT, validateDelete, deleteUser);
+router.post("/", validateCreate, createUser);
+router.put("/:id", validateUpdate, updateUser);
+router.delete("/:id", validateDelete, deleteUser);
 
 export default router;
